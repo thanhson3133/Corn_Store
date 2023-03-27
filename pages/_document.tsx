019354e87a55779af39e7a429a25aa9ebb9bb755
thanhson3133 <thanhson3133@gmail.com>
2023-03-27 11:13:30 +0700

    update meta messenger plugin

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,48 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head></Head>
       <body>
+        {/* <!-- Messenger Chat plugin Code --> */}
+        <div id="fb-root"></div>
+
+        {/* <!-- Your Chat plugin code --> */}
+        <div id="fb-customer-chat" className="fb-customerchat"></div>
+        <Script
+          id="messenger-tag"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `{var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "102447729479085");
+            chatbox.setAttribute("attribution", "biz_inbox");}`,
+          }}
+        ></Script>
+        <Script
+          id="messenger-sdk"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `{window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v16.0'
+              });
+            };
+      
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));}`,
+          }}
+        ></Script>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
